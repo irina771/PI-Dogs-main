f0e6bd123560fc50585dfd861af5b1ae4cd65586
Irina Dominguez <Irina.Dominguez0725@jala.university>
2026-06-05 19:11:02 -0300

    Fix Card component styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,15 +5,15 @@ export default function Card({ name, image, temperament, weight }) {
   console.log(image);
   return (
     <div>
-      <div className={`homeCard`}>
-        <div className={`card-text`}>
-          <div className={`letraTitulo`}>
+      <div className={styles`homeCard`}>
+        <div className={styles`card-text`}>
+          <div className={styles`letraTitulo`}>
             <h3>{name}</h3>
           </div>
           <div>
           <img src={image} alt="img" />
           </div>
-          <div className={`letra`}>
+          <div className={styles`letra`}>
           <h4>Peso min y max: </h4>
           <p>{weight}</p>
           <h4>Temperamentos:</h4>
